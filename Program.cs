5213a01a3d208a489939b617acc7ed6d0d8593f2
Jason Messler <jason.messler.1@gmail.com>
2021-01-25 21:14:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercise_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            do
+            {
+
+                Console.Write("Enter a number ");
+                var input = Console.ReadLine();
+                var userInput = int.Parse(input);
+
+                Console.WriteLine($"The product of { userInput}, { userInput - 1}, and {userInput - 2} is  {(userInput) * (userInput - 1) * (userInput - 2)}");
+
+                Console.WriteLine("would you like to continue (y/n) ");
+                answer = Console.ReadLine();
+
+            } while (answer == "y");
+
+            Console.WriteLine("Goodbye!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
